767b6e688eba60f8eef66fea88905aa84232680a
David Rodriguez <David.Rod1865@gmail.com>
2022-06-12 12:37:25 -0400

    Added Fading Effect

---
+++ rockypolography/coming-soon/index.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    window.setTimeout(fadein, 1400); //1.4 seconds
+  }
+  
+  function fadein() {
+    document.getElementById('fadein').style.opacity = '1';
+  }
+
+
+
